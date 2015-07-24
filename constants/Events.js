261447d2790b33c59bb04d2c57a9e293b6492c5e
Arild Wanvik Tvergrov <arildt@mesan.no>
2015-07-24 13:57:33 +0200

    Document WebSocket events so far.

--- constants/Events.js
+++ constants/Events.js
@@ -1,10 +1,16 @@
 import keyMirror from 'keymirror';
 
 export default keyMirror({
-    connection: null,
-    disconnect: null,
-    tokenRequested: null,
-    tokenSent: null,
-    opponentEnlisted: null,
-    opponentFound: null
+
+    connection: null, // A client connects.
+
+    disconnect: null, // A client disconnects.
+
+    tokenRequested: null, // The server requests the user token from a client.
+
+    tokenSent: null, // The client sends his user token to the server.
+
+    opponentEnlisted: null, // The player enlists for a match.
+
+    opponentFound: null // The server informs the player that an opponent has been found.
 });
