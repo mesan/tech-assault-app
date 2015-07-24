b35d858b9ee8886c1f6a6fa8c3179325b42bf53d
Arild Wanvik Tvergrov <arildt@mesan.no>
2015-07-24 14:10:52 +0200

    Re-enlist players if their opponent has disconnected upon matching.

--- timedEventHandlers/findMatchesEventHandler.js
+++ timedEventHandlers/findMatchesEventHandler.js
@@ -1,28 +1,44 @@
 import requestMatches from '../util/requests/requestMatches';
+import requestPostEnlistment from '../util/requests/requestPostEnlistment';
 import Events from '../constants/Events';
 
+/**
+ * At intervals, the system checks for players that have enlisted themselves for matches. The system matches players
+ * one-on-one. If a player is not connected when he/she is matched with another, the other player is immediately
+ * re-enlisted.
+ */
 export default function findMatchesEventHandler() {
     requestMatches()
         .then((matches) => {
             for (let i = 0; i < matches.length; i++) {
                 const match = matches[i];
 
-                const socketsInMatch = match.map(userToken => this.tokenSocketMap[userToken]);
+                const [userToken1, userToken2] = match;
 
-                if (typeof socketsInMatch[0] === 'undefined') {
-                    console.error(`Could not find socket with token ${match[0]}`);
+                const [socket1, socket2] = match.map(userToken => this.tokenSocketMap[userToken]);
+
+                if (typeof socket1 === 'undefined') {
+                    console.error(`Could not find socket with token ${userToken1}`);
+
+                    if (typeof socket2 !== 'undefined') {
+                        requestPostEnlistment(socket2.token);
+                    }
+
                     continue;
                 }
 
-                if (typeof socketsInMatch[1] === 'undefined') {
-                    console.error(`Could not find socket with token ${match[1]}`);
+                if (typeof socket2 === 'undefined') {
+                    console.error(`Could not find socket with token ${userToken2}`);
+
+                    requestPostEnlistment(socket1.token);
+
                     continue;
                 }
 
                 // TODO: Initialize match (generate game board, fetch players' primary decks, etc).
 
-                socketsInMatch[0].emit(Events.opponentFound, socketsInMatch[1].token);
-                socketsInMatch[1].emit(Events.opponentFound, socketsInMatch[0].token);
+                socket1.emit(Events.opponentFound);
+                socket2.emit(Events.opponentFound);
             }
         });
 }
