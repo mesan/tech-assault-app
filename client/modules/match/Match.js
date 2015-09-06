5788e07d233dd7bff494d68ac55e4d306df00943
Arild Wanvik Tvergrov <arildt@mesan.no>
2015-09-06 18:03:08 +0200

    Remove temporary listing of available cards and positions, previously used for testing purposes.

--- client/modules/match/Match.js
+++ client/modules/match/Match.js
@@ -25,15 +25,6 @@ export default class Match extends React.Component {
 		return (
 			<div>
                 <div id="game"></div>
-                <p>Battle!</p>
-                <p>{players[0].name} <strong>VS</strong> {players[1].name}</p>
-                <button onClick={this.submit.bind(this)}>Perform turn</button>
-                <ul>
-                    {primaryDeck.map(card => <li id={card.id} onClick={this.selectCard.bind(this)}>{card.id}</li>)}
-                </ul>
-                <ul>
-                    {tiles.map((tile) => <li id={tile} onClick={this.selectPosition.bind(this)}>{tile}</li>)}
-                </ul>
             </div>
 		)
 	}
