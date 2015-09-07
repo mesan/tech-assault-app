1687f448cff3388725c59a6bd812682bae1fca30
Jan Eirik <janeirik@hallstensen.net>
2015-09-07 18:04:34 +0200

    Basic styling for highscores

--- client/modules/highscore/Highscore.js
+++ client/modules/highscore/Highscore.js
@@ -26,13 +26,16 @@ export default class Highscore extends React.Component {
 	}
 
 	renderScore(playerScore) {
+
+		const userAvatar = playerScore.avatar ? playerScore.avatar.large : 'http://gjesteurl';
+
 		const { playerName, rank, score } = playerScore;
 
 		return (
-			<div class="rank-item">
-				<img src= />
-				{playerName}
-				{rank}st / {score}p
+			<div>
+				<img className="highscore-avatar" src={userAvatar} />
+				<div><span className="highscore-name">{playerName}</span></div>
+				<div><span className="highscore-rank">{rank}</span>st / <span className="highscore-score">{score}</span>p</div>
 			</div>
 		);
 	}
