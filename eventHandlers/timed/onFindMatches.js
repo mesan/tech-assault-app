373ea12db6431a5087e57bc58fe43d9155ec7be8
Arild Wanvik Tvergrov <arildt@mesan.no>
2015-09-07 09:23:05 +0200

    Pass match interval map to onTurnTimedOut event call.

--- eventHandlers/timed/onFindMatches.js
+++ eventHandlers/timed/onFindMatches.js
@@ -73,7 +73,7 @@ export default function onFindMatches() {
                                     return tokenSocketMap[token];
                                 });
 
-                                onTurnTimeout(sockets, initialCountdown, match.nextTurn);
+                                onTurnTimeout(sockets, initialCountdown, match.nextTurn, matchIntervalMap);
                             }
                         });
                     })
