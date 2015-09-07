3bf6ad25bd4be97e08bdfa2bc24f31c49d3cdc8c
Arild Wanvik Tvergrov <arildt@mesan.no>
2015-09-07 22:51:28 +0200

    List all environment variables required to launch this module.

--- server.js
+++ server.js
@@ -2,9 +2,27 @@ import Hapi from 'hapi';
 import fs from 'fs';
 import socketApp from './socketApp';
 
-let {
+const envVars = [
+    'TECH_DOMAIN_ENDPOINT',
+    'TECH_AUTH_ENDPOINT'
+];
+
+let undefinedEnvVars = [];
+
+for (let envVar of envVars) {
+    if (typeof process.env[envVar] === 'undefined') {
+        undefinedEnvVars.push(envVar);
+    }
+}
+
+if (undefinedEnvVars.length > 0) {
+    console.error(`You need to define the following environment variable(s): ${undefinedEnvVars.join(', ')}`);
+    process.exit(1);
+}
+
+const {
     TECH_AUTH_ENDPOINT
-} = process.env;
+    } = process.env;
 
 let server = new Hapi.Server();
 
