cad87dd95f61dc1c50d7f6d30fc80f27e1ad06e7
Jan Eirik <janeirik@hallstensen.net>
2015-09-08 17:05:08 +0200

    Display players score and rank when in match

--- src/client/modules/match/Match.js
+++ src/client/modules/match/Match.js
@@ -7,6 +7,18 @@ export default class Match extends React.Component {
 	render() {
 		return (
 			<div>
+                <div>
+                    <ul>
+                        <li>Name: {this.props.match.players[0].name}</li>
+                        <li>Score: {this.props.match.players[0].highscore.score}</li>
+                        <li>Rank: {this.props.match.players[0].highscore.rank}</li>
+                    </ul>
+                    <ul>
+                        <li>Name: {this.props.match.players[1].name}</li>
+                        <li>Score: {this.props.match.players[1].highscore.score}</li>
+                        <li>Rank: {this.props.match.players[1].highscore.rank}</li>
+                    </ul>
+                </div>
                 <Countdown secondsLeft={this.props.secondsLeft} />
                 <MatchBoard
                     match={this.props.match}
