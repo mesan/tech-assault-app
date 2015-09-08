4efa56e83b0e14d46606f33bc3c836b04019fe59
Arild Wanvik Tvergrov <arildt@mesan.no>
2015-09-08 21:58:37 +0200

    Style login screen.

--- src/client/modules/login/Login.js
+++ src/client/modules/login/Login.js
@@ -4,9 +4,10 @@ export default class Login extends React.Component {
 	
 	render() {
 		return (
-			<section>
+			<section className="page-login">
 				<h1>Login</h1>
-				<a href="/login?provider=twitter">Log in with Twitter</a>
+				<img className="login-twitter-image" src="images/twitter.png" />
+				<a className="btn" href="/login?provider=twitter">Log in with Twitter</a>
 			</section>
 		);
 	}
