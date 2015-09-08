93b86f44ef45f9e7371fe3c37142520beda996ef
Arild Wanvik Tvergrov <arildt@mesan.no>
2015-09-08 22:22:22 +0200

    Style splash screen.

--- src/client/modules/splash/Splash.js
+++ src/client/modules/splash/Splash.js
@@ -1,12 +1,11 @@
 import React from 'react';
+import Spinner from '../common/Spinner';
 
 export default class Splash extends React.Component {
 
     render() {
         return (
-            <section>
-                <p>TechAssault loading ...</p>
-            </section>
+            <Spinner text="Bootstrapping..." />
         );
     }
 }
