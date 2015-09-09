c995fab6a28db0b6cbf88098f273e5b79895133a
Arild Wanvik Tvergrov <arildt@mesan.no>
2015-09-09 21:42:29 +0200

    Show spacerail text in golden letters.

--- src/presenter/modules/presenter-screens/PresenterScreens.js
+++ src/presenter/modules/presenter-screens/PresenterScreens.js
@@ -8,7 +8,7 @@ export default class PresenterScreens extends React.Component {
                 <h1 className="text-center highscore-header">
                     Want to Play? Visit: <a className="link" href="http://play.mesan.no">play.mesan.no</a>
                 </h1>
-                <h1 className="in-your-face">Fight and win a SpaceRail!</h1>
+                <h1 className="in-your-face">Fight and win a <span className="golden">SpaceRail</span>!</h1>
                 <img className="screen" src="screen1.png" alt="" />
             </section>
         );
