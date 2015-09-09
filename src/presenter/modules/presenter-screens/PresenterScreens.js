d0788d0c578ff87dc151f00629de1cee5cda2393
Arild Wanvik Tvergrov <arildt@mesan.no>
2015-09-09 17:21:41 +0200

    Show play.mesan.no on all presenter pages.

--- src/presenter/modules/presenter-screens/PresenterScreens.js
+++ src/presenter/modules/presenter-screens/PresenterScreens.js
@@ -5,7 +5,10 @@ export default class PresenterScreens extends React.Component {
     render() {
         return (
             <section className="page page-presenter-screens">
-                <h1>Fight and win a SpaceRail!</h1>
+                <h1 className="text-center highscore-header">
+                    Want to Play? Visit: <a className="link" href="http://play.mesan.no">play.mesan.no</a>
+                </h1>
+                <h1 className="in-your-face">Fight and win a SpaceRail!</h1>
                 <img className="screen" src="screen1.png" alt="" />
             </section>
         );
